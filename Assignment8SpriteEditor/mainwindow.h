7e88696eaaafe519dc67e084878e1438ebfc8ff4
Eef <u1282742@umail.utah.edu>
2025-03-30 22:14:42 -0600

    Rectangle/Ellipse implemented

--- Assignment8SpriteEditor/mainwindow.h
+++ Assignment8SpriteEditor/mainwindow.h
@@ -97,6 +97,8 @@ private slots:
     void on_brushBttn_clicked();
     void on_eraseBttn_clicked();
     void on_eyeBttn_clicked();
+    void on_rectangleBttn_clicked();
+    void on_ellipseBttn_clicked();
 
     // New Button
     void on_newButton_clicked();
